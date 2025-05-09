57f80eadf4ad7859c7fe9813132adbfe4385abbf
dbunning24 <dan.bunning24@bathspa.ac.uk>
2025-05-09 17:01:54 +0100

    started 2nd calculation

--- sql/fetch.sql
+++ sql/fetch.sql
@@ -26,9 +26,43 @@ select s.system_name as system,
         (select pv.party, round(pv.votes / cast(sum(pv.votes) over() as float) * 100.0, 2) 
             as [%votes] from party_votes pv
         ) vp
-    where s.id = 1 
+    where s.id = 1
         and p.party_id = v.party_id
         and pv.party = p.party_id 
         and sp.party_id = pv.party 
         and vp.party = sp.party_id;
-        
+
+select s.system_name as system, 
+        p.party_name as party, 
+        v.seats, 
+        sp.[%seats],
+        vp.[%votes],
+        IIF(
+            sp.[%seats] < vp.[%votes], 
+            "-"||(select vp.[%votes] - sp.[%seats]),
+            "+" || (select sp.[%seats] - vp.[%votes])
+        ) as [difference between percentage of votes and seats],
+        (select p.party_name from 
+            parties p, 
+            (select party_id, max(seats) 
+                from party_seats
+            ) w 
+            where p.party_id = w.party_id
+        ) as [winning party],
+        ((select max(seats) from party_seats) - v.seats) as [seat difference from winner]
+    from systems s, 
+        party_seats v, 
+        parties p, 
+        party_votes pv,
+        (select v.party_id, round(v.seats / cast(sum(v.seats) over() as float) * 100.0, 2) 
+            as [%seats] from party_seats v
+        ) sp, 
+        (select pv.party, round(pv.votes / cast(sum(pv.votes) over() as float) * 100.0, 2) 
+            as [%votes] from party_votes pv
+        ) vp
+    where s.id = 2
+        and p.party_id = v.party_id
+        and pv.party = p.party_id 
+        and sp.party_id = pv.party 
+        and vp.party = sp.party_id;
+
